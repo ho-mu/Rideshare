3f06710a47ec37e2f7cd6aaf2baa610942663c03
Jason Boser <jasonboser@gmail.com>
2017-06-12 11:30:51 -0500

    initial calendar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
+import Calendar from './Calendar';
+
+import './../styles/styles.css';
+import './../styles/react-big-calendar.css';
+
 class App extends Component {
   render() {
     return (
-      <div>Hello</div>
+        <div id='app'>
+            <h1>NM Rideshare</h1>
+            <Calendar />
+        </div>
     );
   }
 }
