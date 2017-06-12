92b185bbf98909f83021a4b80e5dd1f4d75a519e
WRIGHT, SCOTT <scottwright@northwesternmutual.com>
2017-06-12 13:37:21 -0500

    using boiler plate

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,103 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Route, BrowserRouter, Link, Redirect, Switch } from 'react-router-dom'
+import Login from './Login'
+import Register from './Register'
+import Home from './Home'
+import Dashboard from './protected/Dashboard'
+import { logout } from '../helpers/auth'
+import { firebaseAuth } from '../config/constants'
 
-class App extends Component {
+function PrivateRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === true
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+    />
+  )
+}
+
+function PublicRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === false
+        ? <Component {...props} />
+        : <Redirect to='/rides' />}
+    />
+  )
+}
+
+export default class App extends Component {
+  state = {
+    authed: false,
+    loading: true,
+  }
+  componentDidMount () {
+    this.removeListener = firebaseAuth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+          loading: false,
+        })
+      } else {
+        this.setState({
+          authed: false,
+          loading: false
+        })
+      }
+    })
+  }
+  componentWillUnmount () {
+    this.removeListener()
+  }
   render() {
-    return (
-      <div>Hello</div>
+    return this.state.loading === true ? <h1>Loading</h1> : (
+      <BrowserRouter>
+        <div>
+          <nav className="navbar navbar-default navbar-static-top">
+            <div className="container">
+              <div className="navbar-header">
+                <Link to="/" className="navbar-brand">Home</Link>
+              </div>
+              <ul className="nav navbar-nav pull-right">
+                <li>
+                  <Link to="/" className="navbar-brand">Home</Link>
+                </li>
+                <li>
+                  <Link to="/rides" className="navbar-brand">Dashboard</Link>
+                </li>
+                <li>
+                  {this.state.authed
+                    ? <button
+                        style={{border: 'none', background: 'transparent'}}
+                        onClick={() => {
+                          logout()
+                        }}
+                        className="navbar-brand">Logout</button>
+                    : <span>
+                        <Link to="/login" className="navbar-brand">Login</Link>
+                        <Link to="/register" className="navbar-brand">Register</Link>
+                      </span>}
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div className="container">
+            <div className="row">
+              <Switch>
+                <Route path='/' exact component={Home} />
+                <PublicRoute authed={this.state.authed} path='/login' component={Login} />
+                <PublicRoute authed={this.state.authed} path='/register' component={Register} />
+                <PrivateRoute authed={this.state.authed} path='/rides' component={Dashboard} />
+                <Route render={() => <h3>No Match</h3>} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './index.css';
 
 
 
-//add helper functions for firebase
-const database = require('./firebase/index.js');
+// //add helper functions for firebase
+// const database = require('./helpers/firebase.js');
 
-//load dummy rides data for month of JUNE, 2017
-const {rides} = require('./dummydata.json'); 
+// //load dummy rides data for month of JUNE, 2017
+// const {rides} = require('./dummydata.json'); 
 
-//Save to Feb, 2010 
-database.setMonth(2010,2,rides)
-.then((res)=>{console.log("saved some data")})
-.catch((e)=>{console.log(e)})
+// //Save to Feb, 2010 
+// database.setMonth(2010,2,rides)
+// .then((res)=>{console.log("saved some data")})
+// .catch((e)=>{console.log(e)})
 
-//get the data from Feb, 2010
-database.getMonth(2010,2)
-.then((response)=>{
-    console.log("Got some data")
-    console.log(response.val())
-})
-.catch((e)=>{console.log(e)})
-
-
+// //get the data from Feb, 2010
+// database.getMonth(2010,2)
+// .then((response)=>{
+//     console.log("Got some data")
+//     console.log(response.val())
+// })
+// .catch((e)=>{console.log(e)})
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
