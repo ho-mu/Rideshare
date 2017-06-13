472321604d008870e2b2e7f1c4d9dab33f895b31
WRIGHT, SCOTT <scottwright@northwesternmutual.com>
2017-06-13 13:09:36 -0500

    header container

--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -2,9 +2,8 @@ import {connect} from 'react-redux';
 import Header from './../components/Header';
 
 const mapStateToProps = (state) => {
-    console.log(state)
     return{
-            userName : state.user
+            userName : state.username
     }
 }
 
