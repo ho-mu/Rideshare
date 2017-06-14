e412892a798358dbb5313bfc00c2c4eeeb828c84
WRIGHT, SCOTT <scottwright@northwesternmutual.com>
2017-06-14 13:51:27 -0500

    auto data

--- src/components/App.js
+++ src/components/App.js
@@ -44,8 +44,8 @@ export default class App extends Component {
 
 
       //load data on iniitial mount of app
-         database.getAllTrips()
-        .then((trips)=>{
+         database.subscribeToTripChanges((trips)=>{
+           console.log("got some data")
             const newTrips = Object.keys(trips.val()).map((tripKey=>{
               const newTrip = {...trips.val()[tripKey]}
                 if (!newTrip.passengers)
